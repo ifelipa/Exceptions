1940864311767efe41c24a4797c5da52f78663fd
iaw188216 <iaw188216@h11.informatica.escoladeltreball.org>
2015-10-16 18:43:41 +0200

    +avances

--- Exceptions/src/ejemplosClases/test1.java
+++ Exceptions/src/ejemplosClases/test1.java
@@ -1,13 +1,13 @@
 package ejemplosClases;
 
-public class Main {
+public class test1 {
 
 	public static void main(String[] args) {
-
+		//la condicion previa para que funcione el try/catch es que el programa compile, que sea un main por ejemplo
+		
 		try {
 			System.out.println(5/0);
 		} catch (ArithmeticException e) {
-
 			System.out.println("Vigila, estas dividiendo por cero");
 			
 		}
---
+++ Exceptions/src/ejemplosClases/test2.java
@@ -0,0 +1,38 @@
+package ejemplosClases;
+
+public class test2 {
+
+	public static void main(String[] args) throws Exception {
+				
+		metodo1();
+	}
+	
+	private static void metodo1() throws Exception{
+
+
+		System.out.println("1");
+
+		try {
+			System.out.println("2");
+			
+			metodo2();
+//			if (true)
+//				throw new Exception();
+			System.out.println("3");
+		} catch (Exception e) {
+			System.out.println("4");
+			throw e;
+		} finally {
+			System.out.println("5");
+		}
+		System.out.println("6");
+
+	}
+
+	private static void metodo2() throws Exception {
+		if (true)
+			throw new Exception();
+		
+	}
+
+}
