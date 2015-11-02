55cc234560d5447ca0069f90db98b4e41107de57
isma <ismael.felipa@gmail.com>
2015-11-02 11:51:36 +0100

    +avances repaso

---
+++ Exceptions/src/ejerciciosOpcionesRepaso/Empleat.java
@@ -0,0 +1,10 @@
+package ejerciciosOpcionesRepaso;
+
+public class Empleat extends Persona {
+	int codi;
+	String carrec;
+	int jornadaLaboral;
+	String estudis;
+	double salari;
+	int telefon;
+}
---
+++ Exceptions/src/ejerciciosOpcionesRepaso/Persona.java
@@ -0,0 +1,8 @@
+package ejerciciosOpcionesRepaso;
+
+public class Persona {
+	String nom;
+	String cognom1;
+	String cognom2;
+	 String dni;
+}
